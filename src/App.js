d457b36c95fa9cf2fb3433ffa555d618b0fb13f1
Sara Wilcox <saraewilcox@gmail.com>
2020-11-24 19:06:30 +0000

    done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar.js';
+import FormField from './components/FormField.js';
+import CoolButton from './components/CoolButton.js';
 
 const App = () => {
-  return <h1>Bulma</h1>;
+  return (
+    <div>
+      <Navbar />
+      <section>
+        <FormField label="Name" type="text" placeholder="e.g. Alex Smith" />
+        <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
+        <CoolButton isSuccess text="Submit" />
+      </section>
+    </div>  
+  );
 };
 
 export default App;
